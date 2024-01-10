48062b6ad475f4c792b47049ec259cdb6f9ac978
vinaypullela <151743152+vinaypullela@users.noreply.github.com>
2024-01-10 09:20:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+      message: "type your URL:",
+      name: "URL",
+    },
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr_image.png"));
+
+    fs.writeFile("URL.txt", url, (err) => {
+      // Corrected to use 'url' instead of 'URL'
+      if (err) throw err;
+      console.log("File has been saved!");
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
